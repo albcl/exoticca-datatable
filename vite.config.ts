34e08e865825d9971a5b3944668be0eee3302577
albcl <a.cerveto@gmail.com>
2023-11-07 12:51:27 +0100

    First commit

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference types="vitest" />
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'happy-dom'
+  }
+});
